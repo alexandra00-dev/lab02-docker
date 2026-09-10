28626ea257d3905d902d3def07e80853b3901c3f
Alexandra <alecha0015@gmail.com>
2026-09-09 22:39:16 -0500

    feat: configurar API y Docker Compose

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  
+
   const responseData = {
     status: 'success',
     message: 'API respondiendo correctamente',
